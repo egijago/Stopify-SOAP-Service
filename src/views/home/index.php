feb66afad70f1113bd1f5fdcdf6b854bc3481a0a
vierifirdaus <13521099@mahasiswa.itb.ac.id>
2023-10-01 13:30:18 +0700

    feat : auth(login,register)

---
+++ src/views/home/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../public/css/home.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="sidebar">
+            <div class="logo">Logo</div>
+        </div>
+        <div class="homepage">
+            <div class="user-info">
+                <div class="user-icon">
+                    <i class="fas fa-user"></i>
+                </div>
+                <h3>vieri</h3>
+            </div>
+            <h3>Good morning, vieri</h3>
+            <h3>Recommended for you</h3>
+            <div class="recommended-container">
+                <div class="top-song-first">
+                    <img class="first" src="../../../public/image/senja.jpg" />
+                    <h3>Senja</h3>
+                    <p>Maliq</p>
+                </div>
+                <div class="top-song-second">
+                    <div class="song-item">
+                        <h3>item 1</h3>
+                    </div>
+                    <div class="song-item">
+                        <h3>item 2</h3>
+                    </div>
+                    <div class="song-item">
+                        <h3>item 3</h3>
+                    </div>
+                    <div class="song-item">
+                        <h3>item 4</h3>
+                    </div>
+                </div>
+            </div>
+            <h3>New Realase Song</h3>
+            <div class="new-song">
+                <div class="new-song-item">
+                    <img class="new-song" src="../../../public/image/senja.jpg" />
+                    <h3>Senja</h3>
+                    <p>Maliq</p>
+                </div>
+                <div class="new-song-item">
+                    <img class="new-song" src="../../../public/image/senja.jpg" />
+                    <h3>Senja</h3>
+                    <p>Maliq</p>
+                </div>
+                <div class="new-song-item">
+                    <img class="new-song" src="../../../public/image/senja.jpg" />
+                    <h3>Senja</h3>
+                    <p>Maliq</p>
+                </div>
+                <div class="new-song-item">
+                    <img class="new-song" src="../../../public/image/senja.jpg" />
+                    <h3>Senja</h3>
+                    <p>Maliq</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- src/views/login/index.php
+++ src/views/login/index.php
@@ -1,1 +1,38 @@
-<!-- show page using html css -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../public/css/register.css">
+    <title>Register</title>
+</head>
+<body>
+    <div class="container">
+        <div class="register">
+            <div class="title">
+                <h1>Log in to Stopify</h1>
+            </div>
+            <div class="form">
+                <form action="../../api/auth/login.php" method="post">
+                    <div class="form_input">
+                        <label for="email">Email</label>
+                        <input type="text" name="email" id="email" placeholder="Enter your email ">
+                    </div>
+                    <div class="form_input">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password" placeholder="Enter your password">
+                    </div>
+                    
+                    
+                    <div class="submit_form">
+                        <input type="submit" name="submit" value="Log in">
+                    </div>
+                    <p>Don't have an account?<a href="login.php">Sign up for spotify</a></p>
+                    
+                </form>
+            </div>
+        </div>
+    </div>
+    
+</body>
