8d5a785be23d8b02c2df62f3fecfbef3928d1971
egijago <16521433@mahasiswa.itb.ac.id>
2023-10-02 13:41:45 +0700

    feat: seeder for db

--- migrate/ddl.sql
+++ migrate/ddl.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE stopify;
+\c stopify
 CREATE TABLE users(
     id_user SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
@@ -25,7 +26,7 @@ CREATE TABLE album(
     id_artist INT NOT NULL,
     image_url VARCHAR(255) NOT NULL,
 
-    CONSTRAINT fk_id_artist FOREIGN KEY (id_artist) REFERENCES artist(id_artist)
+    CONSTRAINT fk_id_artist FOREIGN KEY (id_artist) REFERENCES artist(id_artist) ON DELETE CASCADE
 );
 
 
@@ -36,15 +37,15 @@ CREATE TABLE music(
     audio_url VARCHAR(255) NOT NULL,
     id_album INT NOT NULL,
 
-    CONSTRAINT fk_id_genre FOREIGN KEY (id_genre) REFERENCES genre(id_genre),
-    CONSTRAINT fk_id_album FOREIGN KEY (id_album) REFERENCES album(id_album)
+    CONSTRAINT fk_id_genre FOREIGN KEY (id_genre) REFERENCES genre(id_genre) ON DELETE CASCADE,
+    CONSTRAINT fk_id_album FOREIGN KEY (id_album) REFERENCES album(id_album) ON DELETE CASCADE
 );
 
 CREATE TABLE likes(
     id_user INT NOT NULL,
     id_music INT NOT NULL,
-    CONSTRAINT fk_id_user FOREIGN KEY (id_user) REFERENCES users(id_user),
-    CONSTRAINT fk_id_music FOREIGN KEY (id_music) REFERENCES music(id_music)
+    CONSTRAINT fk_id_user FOREIGN KEY (id_user) REFERENCES users(id_user) ON DELETE CASCADE,
+    CONSTRAINT fk_id_music FOREIGN KEY (id_music) REFERENCES music(id_music) ON DELETE CASCADE
 );
 
 
