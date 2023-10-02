1b44ff12d96f39a107b94f7c49bc2198a620a919
vierifirdaus <13521099@mahasiswa.itb.ac.id>
2023-10-02 12:21:59 +0700

    feat : models

---
+++ src/models/likes.php
@@ -0,0 +1,28 @@
+<!-- getAlbumByID -->
+<?php
+include('../db/Database.php');
+Class Likes {
+    private $table = 'likes';
+    private $db;
+  
+    public function __construct()
+    {
+      $this->db = new Database();
+    }
+  
+    public function likes($data)
+    {
+      $this->db->query('INSERT INTO ' . $this->table . ' (id_user, id_music) VALUES (:id_user, :id_music)');
+      $this->db->bind('id_user', $data['id_user']);
+      $this->db->bind('id_music', $data['id_music']);
+      return $this->db->rowCount();
+    }
+    public function dislikes($data)
+    {
+        $this->db->query('DELETE FROM ' . $this->table . ' WHERE id_user = :id_user AND id_music = :id_music');
+        $this->db->bind('id_user', $data['id_user']);
+        $this->db->bind('id_music', $data['id_music']);
+        return $this->db->resultSet();
+    }
+  
+  }
---
+++ src/models/music.php
@@ -0,0 +1,53 @@
+<!-- getAlbumByID -->
+<?php
+include('../db/Database.php');
+Class Music {
+    private $table = 'music';
+    private $db;
+  
+    public function __construct()
+    {
+      $this->db = new Database();
+    }
+  
+    public function getAllMusic()
+    {
+      $this->db->query('SELECT * FROM ' . $this->table);
+      return $this->db->resultSet();
+    }
+  
+    public function getMusicByMusicId($id)
+    {
+      $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id_music = :id');
+      $this->db->bind('id_music', $id);
+      return $this->db->single();
+    }
+    public function editMusic($data){
+      $this->db->query('UPDATE ' . $this->table . ' SET title = :title, id_genre = :id_genre, audio_url = :audio_url, id_album = :id_album WHERE id_music = :id_music');
+      $this->db->bind('title', $data['title']);
+      $this->db->bind('id_genre', $data['id_genre']);
+      $this->db->bind('audio_url', $data['audio_url']);
+      $this->db->bind('id_album', $data['id_album']);
+      $this->db->bind('id_music', $data['id_music']);
+      $this->db->execute();
+      return $this->db->rowCount();
+    }
+  
+    public function insertMusic($data) {
+        $this->db->query('INSERT INTO ' . $this->table . ' (title, id_genre, audio_url, id_album, id_music) VALUES (:title, :id_genre, :audio_url, :id_album, :id_music)');
+        $this->db->bind('title', $data['title']);
+        $this->db->bind('id_genre', $data['id_genre']);
+        $this->db->bind('audio_url', $data['audio_url']);
+        $this->db->bind('id_album', $data['id_album']);
+        $this->db->bind('id_music', $data['id_music']);
+        $this->db->execute();
+        return $this->db->rowCount();
+      }
+  
+    public function deleteMusic($id){
+      $this->db->query('DELETE FROM ' . $this->table . ' WHERE id_music = :id');
+      $this->db->bind('id', $id);
+      $this->db->execute();
+      return $this->db->rowCount();
+    }
+  }
