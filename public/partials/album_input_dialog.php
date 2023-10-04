fc94aeccbab88fb137551cc9721c59a9e83df559
egijago <16521433@mahasiswa.itb.ac.id>
2023-10-04 12:47:48 +0700

    test: try connect to Api

--- public/partials/album_input_dialog.php
+++ public/partials/album_input_dialog.php
@@ -4,27 +4,13 @@ function AlbumInputDialog() {
   $html = <<< "EOT"
     <div class="dialog-wrapper">
       <div class="dialog">
-        <form>
-          <label for="album-title">Album title</label><br>
-          <input type="text" id="album-title" name="album-title"><br>
-          <label for="artist">Artist</label><br>
-          <select id="artist" name="artist">
-            <option value="john doe">John Doe</option>
-            <option value="john doe">Josephine</option>
-            <option value="john doe">John Doe</option>
-            <option value="john doe">Josephine</option>
-            <option value="john doe">John Doe</option>
-            <option value="john doe">Josephine</option>
-          </select><br>
-          <label for="genre">Genre</label><br>
-          <select id="genre" name="genre">
-            <option value="jazz">jazz</option>
-            <option value="jazz">Jjazz</option>
-            <option value="jazz">jazz</option>
-            <option value="jazz">Jjazz</option>
-            <option value="jazz">jazz</option>
-            <option value="jazz">Jjazz</option>
-          </select><br>
+        <form  action="/api/album" method="POST">
+          <label for="title">Album title</label><br>
+          <input type="text" id="album-title" name="title"><br>
+          <label for="album-title">ID Artist</label><br>
+          <input type="text" id="album-title" name="id_artist"><br>
+          <label for="album-title">Image URL</label><br>
+          <input type="text" id="album-title" name="image_url"><br>
           <input type="submit" value="Update">
           <input type="button" onclick="closeDialog()" value="Cancel">
           <input type="submit" value="Delete">
