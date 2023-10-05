201d075b40de7d3fd600f7fa357201b1a310cbf4
vierifirdaus <13521099@mahasiswa.itb.ac.id>
2023-10-05 18:16:48 +0700

    feat : add api

--- src/api/index.php
+++ src/api/index.php
@@ -18,6 +18,7 @@ $router->put('/api/test', function($params) {
 /* Album API */
 $router->get('/api/albums', AlbumController::class .'::getAllAlbum');
 $router->get('/api/album/{id_album}' ,AlbumController::class . '::getAlbumByAlbumId');
+$router->get('/api/albums/records/{current_page}/{limit}' ,AlbumController::class . '::getAlbumRecords');
 $router->put('/api/album/{id_album}', AlbumController::class . '::editAlbum');
 $router->post('/api/album', AlbumController::class . '::insertAlbum');
 $router->delete('/api/album/{id_album}', AlbumController::class . '::deleteAlbum');
@@ -37,17 +38,25 @@ $router->post('/api/genres', GenreController::class . '::insertGenre');
 $router->delete('/api/genres/{id_genre}', GenreController::class . '::deleteGenre');
 
 /* Likes API */
+$router->get('/api/likes/{id_user}', LikesController::class .'::getDetailLikesById');
 $router->post('/api/users/{id_user}/likes/{id_music}', LikesController::class .'::likes');
 $router->delete('/api/users/{id_user}/likes/{id_music}', LikesController::class .'::unlikes');
 
 /* Music API */
 $router->get('/api/musics', MusicController::class .'::getAllMusic');
-$router->get('/api/musics/{id_genre}' ,MusicController::class . '::getMusicByMusicId');
+$router->get('/api/musics/{id_music}' ,MusicController::class . '::getMusicByMusicId');
+$router->get('/api/musics/detail/{id_music}' ,MusicController::class . '::getDetailMusic');
 $router->put('/api/musics', MusicController::class . '::editMusic');
 $router->post('/api/musics', MusicController::class . '::insertMusic');
 $router->delete('/api/musics/{id_genre}', MusicController::class . '::deleteMusic');
 
 /* Users API */
+$router->post('/api/login', UsersController::class .'::login');
+$router->get('/api/users/{id_user}' , UsersController::class .'::getUserById');
+$router->get('/api/users/{username}' , UsersController::class .'::getUserByUsername');
+$router->get('/api/users', UsersController::class . '::getAllUser');
+$router->post('/api/register', UsersController::class . '::insertUser');
+$router->delete('/api/users/{id_user}', UsersController::class . '::deleteUser');
 
 /* Other API  */
 
