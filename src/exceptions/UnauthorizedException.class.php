b767d70d39abe7afdd86adf3622db6e94428eb3e
egijago <16521433@mahasiswa.itb.ac.id>
2023-10-09 11:19:23 +0700

    feat: 401 and 403 exception

--- src/exceptions/UnauthorizedException.class.php
+++ src/exceptions/UnauthorizedException.class.php
@@ -3,7 +3,7 @@ require_once(PROJECT_ROOT_PATH . '/src/exceptions/ClientErrorException.class.php
 class UnauthorizedException extends ClientErrorException 
 {
     protected $RESPONSE_CODE = 401;
-    public function __contruct($message="You cannot access this resource!")
+    public function __contruct($message="You have to login first!")
     {
         parent::__construct($message);
     }
