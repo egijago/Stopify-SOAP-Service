d55e63ae9135ba9c4e157545752efeef9f5259ac
vierifirdaus <13521099@mahasiswa.itb.ac.id>
2023-10-09 13:46:36 +0700

    fix : title

--- src/views/Log%20out/index.php
+++ src/views/Log%20out/index.php
@@ -8,7 +8,7 @@ session_destroy();
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Stopify - Log Out</title>
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
