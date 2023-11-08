3c3d7f22e9f2a9bcc07182b9569c7ab6c381f673
egijago <13521056@mahasiswa.itb.ac.id>
2023-11-08 20:26:24 +0700

    feat: subscription endpoint

---
+++ src/main/java/services/subscription/SubscriptionRepository.java
@@ -0,0 +1,62 @@
+package services.subscription;
+
+import cores.Repository;
+import services.subscription.SubscriptionModel;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubscriptionRepository extends Repository {
+
+    public SubscriptionRepository() {
+        super("subscription");
+    }
+    public List<SubscriptionModel> fetchById(int id) throws SQLException {
+        String sql = "SELECT * FROM " + tableName
+                +" WHERE id_subscription = ?;";
+        List<SubscriptionModel> resultList = new ArrayList<SubscriptionModel>();
+        ResultSet rs = executeQuery(sql, id);
+        while (rs.next()) {
+            SubscriptionModel model = new SubscriptionModel();
+            model.initializeFromResultSet(rs);
+            resultList.add(model);
+        }
+        rs.close();
+        return resultList;
+    }
+
+    public List<SubscriptionModel> fetchAll() throws SQLException {
+        String sql = "SELECT * FROM " + tableName +";";
+        List<SubscriptionModel> resultList = new ArrayList<SubscriptionModel>();
+        ResultSet rs = executeQuery(sql);
+        while (rs.next()) {
+            SubscriptionModel model = new SubscriptionModel();
+            model.initializeFromResultSet(rs);
+            resultList.add(model);
+        }
+        rs.close();
+        return resultList;
+    }
+
+    public int insert(SubscriptionModel subscription) throws SQLException {
+        String sql = "INSERT INTO " + tableName + "(id_artist, id_user) "
+                + "VALUES(?, ?);";
+        int affectedRow = executeUpdate(sql, subscription.getIdArtist(), subscription.getIdUser());
+        return affectedRow;
+    }
+
+    public int update(SubscriptionModel subscription) throws SQLException {
+        String sql = "UPDATE " + tableName + " SET id_artist = ?, id_user = ? WHERE id_subscription = ?";
+        int affectedRow = executeUpdate(sql, subscription.getIdArtist(), subscription.getIdUser(), subscription.getIdSubscription());
+        return affectedRow;
+    }
+
+    public int delete(int id) throws SQLException {
+        String sql = "DELETE FROM " + tableName
+                + " WHERE id_subscription = ?;";
+        int affectedRow = executeUpdate(sql, id);
+        return affectedRow;
+    }
+}
